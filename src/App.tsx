2074a5628ad3620827e958e82061fa09c24589ed
Jackeybai <1303802862@qq.com>
2022-09-23 13:19:15 +0800

    添加了icon并完善了样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,65 @@
-import React from 'react';
-import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import Money from './views/Money';
-import Statistics from './views/Statistics';
-import Tags from './views/Tags';
-import NoMatch from './views/NoMatch';
+import React from "react";
+import Nav from "./components/Nav";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Redirect exact from="/" to="/money"/>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
     </Router>
   );
 }
 
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return <div>页面不存在，你看看是不是整错了</div>;
+}
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,43 +1,51 @@
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import React from 'react';
-import Icon from './Icon';
+import styled from "styled-components";
+import React from "react";
+import { Link } from "react-router-dom";
+require("icons/money.svg");
+require("icons/tag.svg");
+require("icons/chart.svg");
 
 const NavWrapper = styled.nav`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
-    display:flex;
-    > li{
-      width: 33.3333%;
-      text-align:center;
+    display: flex;
+    > li {
+      width: 33.333333%;
+      text-align: center;
       display: flex;
       flex-direction: column;
-      padding: 4px 0;
+      padding: 4px; 0;
       justify-content: center;
       align-items: center;
-      .icon {
-        width: 24px;      
+      > .icon {
+        width: 24px;
         height: 24px;
       }
     }
   }
 `;
 
-const Nav = () => {
+const X = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag"/>
+          <svg fill="skyblue" className="icon">
+            <use xlinkHref="#tag"></use>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <Icon name="money"/>
+          <svg fill="skyblue" className="icon">
+            <use xlinkHref="#money"></use>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
-          <Icon name="chart"/>
+          <svg fill="skyblue" className="icon">
+            <use xlinkHref="#chart"></use>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
@@ -45,4 +53,4 @@ const Nav = () => {
   );
 };
 
-export default Nav;
+export default X;
