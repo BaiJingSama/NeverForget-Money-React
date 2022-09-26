9f2187b496b69d976f2f9aca70fc50e6955d74c4
Jackeybai <1303802862@qq.com>
2022-09-26 15:32:32 +0800

    category 切换类型功能完成

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import React, { useState } from "react";
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   > ul {
     display: flex;
     background: #c4c4c4;
@@ -24,4 +25,26 @@ const CategorySection = styled.section`
   }
 `;
 
-export default CategorySection
+const CategorySection: React.FC = () => {
+  const [categoryList] = useState<("-" | "+")[]>(["-", "+"]);
+  const [category, setCategory] = useState("-"); // +表示收入 -表示支出
+  const categoryMap = { "-": "支出", "+": "收入" };
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map((c) => (
+          <li
+            className={category === c ? "selected" : ""}
+            onClick={() => {
+              setCategory(c);
+            }}
+          >
+            {categoryMap[c]}
+          </li>
+        ))}
+      </ul>
+    </Wrapper>
+  );
+};
+
+export default CategorySection;
