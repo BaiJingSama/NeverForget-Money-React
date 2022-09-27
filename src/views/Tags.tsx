da272d3478af22e53eb9f70acd994b2eee4a52c7
Jackeybai <1303802862@qq.com>
2022-09-27 16:40:22 +0800

    封装findTag方法 到useTags里

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -49,7 +49,7 @@ function Tags() {
       <TagList>
         {tags.map((tag) => (
           <li key={tag.id}>
-            <Link to={"/tags/ " + tag}>
+            <Link to={"/tags/ " + tag.id}>
               <span>{tag.name}</span>
               <Icons name="right" />
             </Link>
