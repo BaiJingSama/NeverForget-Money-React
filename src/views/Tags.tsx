c4097d295a3bb37e0c9cbe164225b75e7243921b
Jackeybai <1303802862@qq.com>
2022-09-27 14:51:59 +0800

    重构代码 将tag: string 重构为 tag:{id:number,name:string}

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -43,14 +43,14 @@ const Space = styled.div`
 `;
 
 function Tags() {
-  const { tags, setTags } = useTags();
+  const { tags } = useTags();
   return (
     <Layout>
       <TagList>
         {tags.map((tag) => (
-          <li key={tag}>
+          <li key={tag.id}>
             <Link to={"/tags/ " + tag}>
-              <span>{tag}</span>
+              <span>{tag.name}</span>
               <Icons name="right" />
             </Link>
           </li>
